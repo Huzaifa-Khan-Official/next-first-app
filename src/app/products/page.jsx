439281c4fb9b48ec00c194372b6efc68641c2052
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-12 20:18:20 +0500

    product rendering done through dummy api

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
-import React from 'react'
+import { notFound } from 'next/navigation';
+import React from 'react';
+import "../style.css"
 
 export const metadata = {
   title: "All Products",
@@ -7,24 +9,52 @@ export const metadata = {
 };
 
 const getData = async () => {
-  const res = await fetch('https://dummyjson.com/products')
+  return new Promise(async (res, rej) => {
+    const result = await fetch('https://dummyjson.com/products')
 
-  console.log(res.json());
-  if (!res.ok) {
-    return null
-  } else {
-    return res
-  }
+    console.log(result.ok);
+    if (result.ok) {
+      const data = await result.json();
+      res(data.products)
+    }
+    // if (!result.ok) {
+    //   rej(null)
+    // } else {
+    //   // res(result.json().products)
+    // }
+  })
+
 }
 
 export default async function page() {
-  const data = await getData()
+  const data = await getData();
+
+  if (!data) {
+    return notFound();
+  }
+
   return (
     <div>
       <h1>Product Page</h1>
 
       <h3>All Products:</h3>
-
+      <div className="cardMainDiv d-flex gap-4 flex-wrap px-2 justify-content-center">
+        {
+          data.map(v => {
+            console.log()
+            return (
+              <div className="card" style={{ "width": "18rem" }}>
+                <img src={v.thumbnail} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{v.title}</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
